1f4d85710472892252552d302078630e7bd65787
anvitha <21jr1a05g0@gmail.com>
2025-09-14 22:22:04 +0530

    stacks

---
+++ stacks.py
@@ -0,0 +1,10 @@
+stack=[]
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print(stack) #print stack
+stack.pop() #pop top ele
+print(stack)
+stack.append(40)
+print(stack[-1]) #peek
+print(len(stack)) #length of stack
